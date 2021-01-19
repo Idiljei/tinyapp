f94025482e62b04a889494068ca79e2286cdab2a
Idiljei <i.jeilani@gmail.com>
2021-01-19 00:26:51 +0000

    express server

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,6 +18,21 @@ app.get("/urls.json", (req, res) => {
 })
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }; //assigned urlDatabase as object in a variable 
+  res.render("urls_index", templateVars);
+});
+
+// app.get("/urls/:b2xVn2", (req, res) => { //use the shortURL for second route
+//   const templateVars = { urls: urlDatabase }; 
+//   res.render("urls_show", templateVars);
+// });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
