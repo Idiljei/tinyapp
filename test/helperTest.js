023d179d5cd0f8773f609ec8a988e4bc98ea0e89
Idiljei <i.jeilani@gmail.com>
2021-01-22 21:33:46 +0000

    helperTest

--- test/helperTest.js
+++ test/helperTest.js
@@ -16,19 +16,28 @@ const testUsers = {
 
 describe("getUserByEmail", function () {
   it("should return a user with valid email", function () {
-    const user = findUserbyEmail("user@example.com", users);
-    const expectedOutput = "userRandomID";
-    assert.equal(user, expectedOutput);
+    const user = findUserbyEmail("user@example.com", testUsers);
+    const expectedOutput = {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur",
+    };
+
+    assert(JSON.stringify(user) === JSON.stringify(expectedOutput));
   });
 
   it("should return a user with valid email", function () {
-    const user = findUserbyEmail("user2@example.com", users);
-    const expectedOutput = "user2RandomID";
-    assert.equal(user, expectedOutput);
+    const user = findUserbyEmail("user2@example.com", testUsers);
+    const expectedOutput = {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "dishwasher-funk",
+    };
+    assert(JSON.stringify(user) === JSON.stringify(expectedOutput));
   });
 
   it("should return undefined", function () {
-    const user = findUserbyEmail("idontexist@example.com",users);
+    const user = findUserbyEmail("idontexist@example.com", testUsers);
     const expectedOutput = undefined;
     assert.equal(user, expectedOutput);
   });
